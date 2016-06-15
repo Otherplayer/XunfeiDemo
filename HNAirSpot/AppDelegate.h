684cdf801494f755136afe2c513b235268c29c14
lel.guo <lel.guo@haihangyun.com>
2016-06-15 16:16:32 +0800

    Initial Commit

---
+++ HNAirSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNAirSpot
+//
+//  Created by __无邪_ on 16/6/15.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HNAirSpot/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HNAirSpot
+//
+//  Created by __无邪_ on 16/6/15.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
